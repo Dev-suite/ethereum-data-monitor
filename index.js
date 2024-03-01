5219f7cb0fb5a43cdccbb232301b6e3a46ce2617
Kamaldeen Sulyman <70143282+Dev-suite@users.noreply.github.com>
2024-03-01 10:06:56 +0100

    Using QuickNode Coingecko Addon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
-// QuickNode RPC endpoint
-const quickNodeRpcEndpoint = `https://nameless-thrumming-waterfall.quiknode.pro/04fccd4e469673f75d9cd284597b504f96bcf5af/`;
+// QuickNode CoinGecko Addon endpoint
+const quickNodeRpcEndpoint = `YOUR_QUICKNODE_COINGECKO_ADDON_ENDPOINT`;
 
 // Function to fetch Ethereum data using QuickNode's RPC endpoint
 async function getEthereumData() {
@@ -17,18 +17,31 @@ async function getEthereumData() {
     const latestBlockNumberHex = blockNumberResponse.data.result;
     const latestBlockNumber = parseInt(latestBlockNumberHex, 16); // Convert hex to decimal
     console.log('Latest Ethereum block number:', latestBlockNumber);
-
-    // Fetch Ethereum price from CoinGecko
-    const ethereumPriceResponse = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
-    const ethereumPrice = ethereumPriceResponse.data.ethereum.usd;
-    console.log('Latest Ethereum price (USD):', ethereumPrice);
   } catch (error) {
     console.error('Error fetching Ethereum data:', error.message);
   }
 }
 
-// Call the function to fetch Ethereum data
+// Function to fetch Ethereum price from CoinGecko using QuickNode CoinGecko Addon
+async function getEthereumPrice() {
+  try {
+    const response = await axios.post(quickNodeRpcEndpoint, {
+      jsonrpc: '2.0',
+      method: 'cg_simplePrice',
+      params: ['ethereum', 'usd'],
+      id: 1,
+    });
+    
+    const ethereumPrice = response.data.result.ethereum.usd;
+    console.log('Latest Ethereum price (USD):', ethereumPrice);
+  } catch (error) {
+    console.error('Error fetching Ethereum price:', error.message);
+  }
+}
+
+// Call the functions to fetch Ethereum data
+getEthereumPrice();
 getEthereumData();
 
 // Show the Ethereum price every 30 seconds
-setInterval(getEthereumData, 30 * 1000); // 30 seconds (30 * 1000 milliseconds)
+setInterval(getEthereumData, getEthereumPrice(), 30 * 1000); // 30 seconds (30 * 1000 milliseconds)
